af2b874328defa42aed607e299a8308411954d2f
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-03-07 00:50:30 +0500

    Chapter2

--- app.js
+++ app.js
@@ -9,5 +9,82 @@ let a = 10;
 console.log(a)
 let = prompt("Hi How are you?", "Great!i, m learning javascript as we speek")
 // Chapter2
+// 3 Kisam ka veriables hota ha
+// let or var veriables change hoskta ha
+// let veriable
+document.write("Let veriables", "<br>")
+let num = ("Talha")
+document.write(num, "<br>")
+document.write("Veriable ke value change ke ha", "<br>")
+num = ("ali")
+document.write(num, "<br>")
+// var veriable
+document.write("var veriable ha", "<br>")
+var alphabat = ("Abid")
+document.write(alphabat, "<br>")
+// const veriable
+document.write("Const veriable ha ya change nai hota", "<br>")
+const num1 = (50)
+document.write(num1, "<br>")
+// const veriable change nhai hota
+// num1 = (30)
+// console.log(num1)
+
+// Primitive Datatype
+// String Number Boolean BigInt Symbol Undefined null
+//  proctice exercise2.1
+document.write("String", "<br>")
+let str1 = ("Owais")
+document.write(str1, "<br>")
+document.write("String ko boolean ma convert kya", "<br>")
+let str2 = Boolean("Amir")
+document.write(str2, "<br>")
+let str3 = (40)
+document.write(str3, "<br>")
+let str4 = (null)
+document.write(str4, "<br>")
+let str5 = (undefined)
+document.write(str5, "<br>")
+// Do number ko addition subtraction multiplicastion divison 
+document.write("Addition Subtraction Multiplication Divison ", "<br>")
+let nrl1 = (30)
+let nrl2 = (10)
+document.write("Addition", nrl1 + nrl2, "<br>")
+document.write("Subtraction", nrl1 - nrl2, "<br>")
+document.write("Multiplication", nrl1 * nrl2, "<br>")
+document.write("Divison", nrl1 / nrl2, "<br>")
+// Exercise 2.2
+document.write("Boleant Ma convert kya", "<br>")
+let name = Boolean("Umar")
+document.write(name, "<br>")
+let age = Boolean()
+document.write(age, "<br>")
+// practice exercise 2.3
+document.write("Increment", "<br>")
+let nbr = (30)
+document.write(nbr, "<br>")
+// increment
+document.write(++nbr, "<br>");
+// decrement
+document.write("Decrement", "<br>")
+let nbr1 = (28)
+document.write(--nbr1, "<br>");
+// Equal
+document.write("Equal", "<br>")
+let nest = ("omair")
+let nast = ("omair")
+// practice 2.4
+document.write(nest === nast, "<br>")
+// Not equal
+document.write("Not equal", "<br>")
+let nest1 = ("omair")
+let nast1 = ("omair")
+document.write(nest1 !== nast1, "<br>")
+document.write("Not equal", "<br>")
+
+let number1 = (5)
+let number2 = (8)
+document.write(number1 > number2)
+
 
 
