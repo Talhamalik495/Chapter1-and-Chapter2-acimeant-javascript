333ca42796461af90f2cecfcad4f92e6485dd4d3
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-03-06 19:54:23 +0500

    Chapter1

---
+++ app.js
@@ -0,0 +1,13 @@
+// Chapter 1 Example 1
+alert("HEllo Sir");
+//  Example 1.2
+console.log("Hello World");
+// Example 1.3
+// Line wise work
+// Example 1.4
+let a = 10;
+console.log(a)
+let = prompt("Hi How are you?", "Great!i, m learning javascript as we speek")
+// Chapter2
+
+
